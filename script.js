808acc239ed88627dcce446b780daa7638a48704
Shashinath jha <128596144+shashi9905@users.noreply.github.com>
2025-05-29 14:47:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-function createTable() {
-    //Write your code here
-  
+function createTable(){
+	let rows = prompt("enter a row")
+	let coulmn = prompt("enter a coulmn")
+	let mytable=document.querySelector("#myTable")
+	for (let i=0;i<rows;i++){
+		let tr=document.createElement("tr")
+		for(let j=0; j<coulmn;j++){
+			let td=document.createElement("td")
+			td.innerText="a"
+			tr.append(td)
+			
+		}
+		mytable.append(tr)
+	}
 }
